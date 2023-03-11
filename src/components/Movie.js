db98fa715b931e6cf409b2dae3eedecff96b89a6
curtyraissa <raissacurty@gmail.com>
2023-03-10 22:26:50 -0300

    criado props no componente movies

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,13 @@
 import styled from "styled-components"
 
-export const Movie = () => {
-    return (
-        <MovieContainer>
-          <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster" />
-        </MovieContainer>
-    )
+export const Movie = ({ src, alt }) => {
+  return (
+    <MovieContainer>
+      <img
+        src={src}
+        alt={alt} />
+    </MovieContainer>
+  )
 }
 
 const MovieContainer = styled.div`
