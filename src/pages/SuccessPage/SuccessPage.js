aec0e15b37c4b904876a39573f813ffbfd3e8300
curtyraissa <raissacurty@gmail.com>
2023-03-10 22:58:46 -0300

    importado componente text e usado link no button para voltar a home

--- src/pages/SuccessPage/SuccessPage.js
+++ src/pages/SuccessPage/SuccessPage.js
@@ -1,33 +1,18 @@
+import { Link } from "react-router-dom"
 import styled from "styled-components"
+import { Text } from "../../components/Text"
 
 export default function SuccessPage() {
 
-    return (
-        <PageContainer>
-            <h1>Pedido feito <br /> com sucesso!</h1>
-
-            <TextContainer>
-                <strong><p>Filme e sessão</p></strong>
-                <p>Tudo em todo lugar ao mesmo tempo</p>
-                <p>03/03/2023 - 14:00</p>
-            </TextContainer>
-
-            <TextContainer>
-                <strong><p>Ingressos</p></strong>
-                <p>Assento 01</p>
-                <p>Assento 02</p>
-                <p>Assento 03</p>
-            </TextContainer>
-
-            <TextContainer>
-                <strong><p>Comprador</p></strong>
-                <p>Nome: Letícia Chijo</p>
-                <p>CPF: 123.456.789-10</p>
-            </TextContainer>
-
-            <button>Voltar para Home</button>
-        </PageContainer>
-    )
+  return (
+    <PageContainer>
+      <h1>Pedido feito <br /> com sucesso!</h1>
+      <Text />
+      <Link to="/">
+        <button>Voltar para Home</button>
+      </Link>
+    </PageContainer>
+  )
 }
 
 const PageContainer = styled.div`
@@ -56,16 +41,5 @@ const PageContainer = styled.div`
         align-items: center;
         text-align: center;
         color: #247A6B;
-    }
-`
-const TextContainer = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin-top: 30px;
-    strong {
-        font-weight: bold;
-        margin-bottom: 10px;
     }
 `
