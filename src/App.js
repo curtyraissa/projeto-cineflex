54188d0737ef90379c67f27ef9ad9377e2b4b284
curtyraissa <raissacurty@gmail.com>
2023-03-12 13:55:53 -0300

    bonus de criar componente header, importado no app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import styled from "styled-components"
 import HomePage from "./pages/HomePage/HomePage"
 import SeatsPage from "./pages/SeatsPage/SeatsPage"
 import SessionsPage from "./pages/SessionsPage/SessionsPage"
 import SuccessPage from "./pages/SuccessPage/SuccessPage"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Header } from "./components/Header"
 
 export default function App() {
-  const nomeSite = "CINEFLEX"
-  
   return (
     <BrowserRouter>
-      <NavContainer>{nomeSite}</NavContainer>
+      <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -21,21 +19,3 @@ export default function App() {
     </BrowserRouter>
   )
 }
-
-const NavContainer = styled.div`
-    width: 100%;
-    height: 70px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #C3CFD9;
-    color: #E8833A;
-    font-family: 'Roboto', sans-serif;
-    font-size: 34px;
-    position: fixed;
-    top: 0;
-    a {
-        text-decoration: none;
-        color: #E8833A;
-    }
-`
