bb6474419184af6c4c0d8ec8f70ea8ba2f8015c6
curtyraissa <raissacurty@gmail.com>
2023-03-12 01:09:20 -0300

    ajustado componente seats e passado props

--- src/components/Seats.js
+++ src/components/Seats.js
@@ -1,26 +1,12 @@
 import styled from "styled-components"
 
-export const Seats = () => {
+export const Seats = ({numAssento}) => {
   return (
-    <SeatsContainer>
-      <SeatItem data-test="seat">01</SeatItem>
-      <SeatItem data-test="seat">02</SeatItem>
-      <SeatItem data-test="seat">03</SeatItem>
-      <SeatItem data-test="seat">04</SeatItem>
-      <SeatItem data-test="seat">05</SeatItem>
-    </SeatsContainer>
+    <>
+      <SeatItem data-test="seat">{numAssento}</SeatItem>
+    </>
   )
 }
-
-const SeatsContainer = styled.div`
-    width: 330px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-    margin-top: 20px;
-`
 
 const SeatItem = styled.div`
     border: 1px solid blue;         // Essa cor deve mudar
