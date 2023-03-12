4f6551863717fb1898234756c07773f26aeb2f8b
curtyraissa <raissacurty@gmail.com>
2023-03-12 16:08:50 -0300

    import component caption

--- src/pages/SeatsPage/SeatsPage.js
+++ src/pages/SeatsPage/SeatsPage.js
@@ -1,11 +1,11 @@
 import axios from "axios"
+import styled from "styled-components"
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
-import styled from "styled-components"
-import { Caption } from "../../components/Caption"
 import { Footer } from "../../components/Footer"
 import { Form } from "../../components/Form"
 import { Seats } from "../../components/Seats"
+import { Caption } from "../../components/Caption"
 
 export default function SeatsPage() {
   const [assento, setAssento] = useState([])
@@ -29,6 +29,10 @@ export default function SeatsPage() {
     })
   }, [])
 
+  // function escolherAssento(id, sentar){
+  //   setAssento([...assento, id])
+  // }
+
   return (
     <PageContainer>
       {selecione}
@@ -39,23 +43,10 @@ export default function SeatsPage() {
         )}
       </SeatsContainer>
 
-      <CaptionContainer>
-        <CaptionItem>
-          <CaptionCircle green />
-          Selecionado
-        </CaptionItem>
-        <CaptionItem>
-          <CaptionCircle />
-          Disponível
-        </CaptionItem>
-        <CaptionItem>
-          <CaptionCircle yellow />
-          Indisponível
-        </CaptionItem>
-      </CaptionContainer>
+      <Caption />
 
       <Form />
-      
+
       <Footer src={filme.posterURL} alt={filme.title}>
         <p>{filme.title}</p>
         <p>{dia.weekday} - {dia.date}</p>
@@ -84,28 +75,4 @@ const SeatsContainer = styled.div`
     align-items: center;
     justify-content: center;
     margin-top: 20px;
-`
-const CaptionContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 300px;
-    justify-content: space-between;
-    margin: 20px;
-`
-const CaptionCircle = styled.div`
-    border: 1px solid blue;         // Essa cor deve mudar
-    background-color: lightblue;    // Essa cor deve mudar
-    height: 25px;
-    width: 25px;
-    border-radius: 25px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 5px 3px;
-`
-const CaptionItem = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-size: 12px;
 `
